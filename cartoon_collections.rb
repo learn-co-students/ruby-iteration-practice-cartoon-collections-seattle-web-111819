fcf40bc034c83f58640884cd3f37ce38ff509acb
jmknopf1007 <jmknopf1007@gmail.com>
2019-11-20 10:31:01 -0800

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,42 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+# prints out the 7 dwarfs in a numbered list
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index| #tests won't pass if index is before name inside the pipes.    
+    index += 1
+    puts "#{index}. #{name}" 
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# returns an array with the same number of elements that it was given
+# capitalizes each element and adds an exclamation mark (test 1)
+# capitalizes each element and adds an exclamation mark (test 2)
+
+
+def summon_captain_planet(planeteer_calls) 
+  planeteer_calls.map do |n|  
+    n.capitalize << "!"  
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# returns true if any calls are longer than 4 characters
+# returns false if all calls are 4 characters or less
+
+def long_planeteer_calls(calls) 
+  calls.any? do |call|
+    call.length > 4  
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+# returns the first element of the array that is cheese
+# returns nil if the array does not contain a type of cheese
+
+#cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+def find_the_cheese(array) 
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end 
 end
+
+
